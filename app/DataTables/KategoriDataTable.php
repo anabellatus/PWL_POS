a5e7bf252de5b0a091a42cc0a02becb1d709308b
anabellatus <anabellatus@gmail.com>
2024-03-27 14:00:01 +0700

    add crud kategori

---
+++ app/DataTables/KategoriDataTable.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\Kategori;
+use App\Models\KategoriModel;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class KategoriDataTable extends DataTable
+{
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->setRowId('id')
+            ->addColumn('action', function ($row) {
+                $editButton = '<a href="' . route('kategori.edit', $row->kategori_id) . '" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i> </a>&nbsp';
+
+                $deleteButton = '<a href="' . route('kategori.delete', $row->kategori_id) . '" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin ingin menghapus kategori ini?\')"><i class="fa fa-trash"></i></a>';
+
+                return $editButton . ' ' . $deleteButton;
+            });
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(KategoriModel $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+            ->setTableId('kategori-table')
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            //->dom('Bfrtip')
+            ->orderBy(0, 'asc')
+            ->selectStyleSingle()
+            ->buttons([
+                Button::make('excel'),
+                Button::make('csv'),
+                Button::make('pdf'),
+                Button::make('print'),
+                Button::make('reset'),
+                Button::make('reload')
+            ]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+            // Column::computed('action')
+            //     ->exportable(false)
+            //     ->printable(false)
+            //     ->width(60)
+            //     ->addClass('text-center'),
+            Column::make('kategori_id'),
+            Column::make('kategori_kode'),
+            Column::make('kategori_nama'),
+            Column::make('created_at'),
+            Column::make('updated_at'),
+            Column::make('action'),
+        ];
+    }
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'Kategori_' . date('YmdHis');
+    }
+}
--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,28 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\DataTables\KategoriDataTable;
+use App\Models\KategoriModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
-    public function index()
+    public function index(KategoriDataTable $dataTable)
     {
-        // $data = [
-        //     'kategori_kode' => 'SNK',
-        //     'kategori_nama' => 'Snack/Makanan Ringan',
-        //     'created_at' => now(),
-        // ];
-        // DB::table('m_kategori')->insert($data);
-        // return 'Insert data baru berhasil';
+        return $dataTable->render('kategori.index');
+    }
 
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']); 
-        // return 'Update data berhasil. Jumlah data yang diupdate: '.$row.' baris';
+    public function create()
+    {
+        return view('kategori.create');
+    }
 
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
-        // return 'Delete data berhasil. Jumlah data yang dihapus: '.$row.' baris';
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
 
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $data]);
+        return redirect('/kategori');
+    }
+
+    public function edit($id)
+    {
+        $kategori = KategoriModel::find($id);
+        return view('kategori.edit', compact('kategori'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $kategori = KategoriModel::find($id);
+        $kategori->kategori_kode = $request->kategori_kode;
+        $kategori->kategori_nama = $request->kategori_nama;
+        $kategori->save();
+
+        return redirect('/kategori')->with('success', 'Kategori berhasil diupdate');
+    }
+    public function delete($id)
+    {
+        $kategori = KategoriModel::find($id);
+        if (!$kategori) {
+            abort(404);
+        }
+
+        $kategori->delete();
+
+        return redirect()->route('kategori.index')->with('success', 'Kategori berhasil dihapus');
     }
 }
