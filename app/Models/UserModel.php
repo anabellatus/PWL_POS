4962411ba322ed832817ba591f23f2376da8d296
Ana Bellatus <57426532+anabellatus@users.noreply.github.com>
2024-05-08 11:25:10 +0700

    js 9 login register

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User;
 
-class UserModel extends Model
+class UserModel extends User
 {
     use HasFactory;
+    use Authenticatable;
 
     protected $table = 'm_user';
     public $timestamps = false;
